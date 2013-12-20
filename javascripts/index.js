360b0548538ce73fe17f2d9ad0a5bb659cd4a67e
unknown <minren@minren-PC0.tencent.com>
2013-12-20 15:01:29 +0800

    访问相册

--- javascripts/index.js
+++ javascripts/index.js
@@ -15,10 +15,32 @@
  */
 
 Jx().$package(function(J){
-	console.log("starting...");
-	var D = Jx().dom;
+	console.log('starting...');
+	var D = Jx().dom,
+		E = Jx().event;
+
+	E.on(D.id('list_album'),'click',function(arguments){
+		console.log('list_album...');
 
 
+	//从页面收集OpenAPI必要的参数
+	var paras = {
+
+	};
+
+	//用jssdk调用OpenAPI
+	QC.api("list_album", paras)
+		.success(function(s){//请自行改写成功回调
+			alert(s.albumnum);
+		})
+		.error(function(f){//请自行改写失败回调
+			alert("分享失败！");
+		})
+		.complete(function(c){//请自行改写完成请求回调
+			alert("分享完成！");
+		});
+
+	});
 
 
 });
